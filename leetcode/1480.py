1e36e955f6ebe762555e6221c50e1a12b80e1cc1
Faizan Jamil <faizan.jamilwork@gmail.com>
2020-09-18 20:56:19 -0400

    finished problem 1480

---
+++ leetcode/1480.py
@@ -0,0 +1,9 @@
+def runningSum(self, nums: List[int]) -> List[int]:
+    result = nums
+    for i in range(0, len(nums)):
+        if (i == 0):
+            continue
+        else:
+            result[i] = result[i] + nums[i-1]
+
+    return result
