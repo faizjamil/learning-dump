bc0882d11aa5f8cda72707f57855b053afc6fa8e
Faizan Jamil <faizan.jamilwork@gmail.com>
2020-10-16 19:31:26 -0400

    connected to mongodb cluster

--- passport.js/app.js
+++ passport.js/app.js
@@ -1,12 +1,22 @@
+const dotenv = require('dotenv').config()
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
 const app = express();
 const PORT = process.env.PORT || 3000;
+const mongoose = require('mongoose');
+// DB config
+// DO NOT USE THE COMMENTED OUT LINE, USE ENV VARIABLES
+//const db = require('./config/keys').MongoURI()
+const db = process.env.MONGODB_URI;
+
+// Connect to Mongo
+mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => console.log('mongodb connected'))
+    .catch(err => console.log(err));
 
 // EJS
 app.use(expressLayouts);
 app.set('view engine', 'ejs');
-
 
 // routes
 
---
+++ passport.js/config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    MongoURI: 'mongodb+srv://test-user:uZc7eroPVteQVCFK@passport-test.djbip.mongodb.net/passport-test?retryWrites=true&w=majority'
+}
