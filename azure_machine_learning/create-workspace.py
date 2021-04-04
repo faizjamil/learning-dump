d74f272ea5e78ca8bd162224675e98422d2ff4de
Faizan <faizan.jamilwork@gmail.com>
2021-04-04 19:57:43 -0400

    created azure workspace

--- azure_machine_learning/create-workspace.py
+++ azure_machine_learning/create-workspace.py
@@ -2,10 +2,10 @@
 from azureml.core import Workspace
 
 ws = Workspace.create(name='ann-training', # provide a name for your workspace
-                      subscription_id='<azure-subscription-id>', # provide your subscription ID
+                      subscription_id='530cc7a9-7825-4d62-95e2-fd187f91e3ff', # provide your subscription ID
                       resource_group='s21-ann', # provide a resource group name
                       create_resource_group=True,
-                      location='useast1') # For example: 'westeurope' or 'eastus2' or 'westus2' or 'southeastasia'.
+                      location='eastus2') # For example: 'westeurope' or 'eastus2' or 'westus2' or 'southeastasia'.
 
 # write out the workspace details to a configuration file: .azureml/config.json
 ws.write_config(path='.azureml')
