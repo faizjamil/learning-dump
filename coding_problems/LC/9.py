388e84b2bed8e166c6d6291906f19135ae5e7c78
Faizan Jamil <faizan.jamilwork@gmail.com>
2021-05-26 21:14:51 -0400

    reorganized folders

---
+++ coding_problems/LC/9.py
@@ -0,0 +1,7 @@
+# link: https://leetcode.com/problems/palindrome-number/
+# check if a number is a palindrome
+# if yes return True else return false
+def isPalindrome(x:int) -> bool:
+    #reverse number and check if it's the same as the original
+    #when handling negative sign, we can assume the number is not a palindrome
+    pass
