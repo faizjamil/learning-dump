2852996d764868f38de3291884315b10e5699d83
Faizan Jamil <faizan.jamilwork@gmail.com>
2021-06-05 13:21:10 -0400

    changed max_sum to be min int value by default

--- coding_problems/HR/2d_array_hourglass_sums.py
+++ coding_problems/HR/2d_array_hourglass_sums.py
@@ -1,5 +1,6 @@
 # link: link: https://www.hackerrank.com/challenges/2d-array/problem
 # parameter: arr- an arbitrary 6 x 6 array
+import sys
 def hourglassSum(arr):
     if (len(arr) < 6 or arr == None):
         return 0
@@ -8,17 +9,31 @@ def hourglassSum(arr):
         # left_edge = 0
         # right_edge = 2
         for i in range(0, len(arr)-2):
-            if len(arr[i] < 6):
+            if len(arr[i]) < 6 :
                 return 0
             for j in range(0, len(arr[i])-2):
                 sums.append(arr[i][j] + arr[i][j+1] + arr[i][j+2] + arr[i+1][j+1] + arr[i+2][j] + arr[i+2][j+1] + arr[i+2][j+2])
-        max_sum = 9
+        max_sum = sys.maxsize*(-1)
         for k in range(0, len(sums)):
             max_sum = max(max_sum, sums[k])
         
         return max_sum
 
+# expectec output is -19
+# 0 -4 -6 0 -7 -6
+# -1 -2 -6 -8 -3 -1
+# -8 -4 -2 -8 -8 -6
+# -3 -1 -2 -5 -7 -4
+# -3 -5 -3 -6 -6 -6
+# -3 -6 0 -8 -6 -7
 # driver code
+arr = [
+    [0,-4,-6,0,-7,-6], 
+    [-1,-2,-6,-8,-3,-1],
+    [-8,-4,-2,-8,-8,-6],
+    [-3,-1,-2,-5,-7,-4],
+    [-3,-5,-3,-6,-6,-6],
+    [-3,-6,0,-8,-6,-7]]
 # arr = [
 #     [1,1,1,0,0,0], 
 #     [0,1,0,0,0,0],
