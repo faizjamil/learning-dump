fe6dea5cacc8a212777d3bfad163e736d359ac55
Faizan Jamil <faizan.jamilwork@gmail.com>
2021-06-19 18:13:11 -0400

    started palindrome linkedlist problem

--- coding_problems/LC/234.py
+++ coding_problems/LC/234.py
@@ -1,1 +1,15 @@
 # link: https://leetcode.com/problems/palindrome-linked-list/
+# testing in python shell where palindrome = "131"
+# >>> print(palindrome[0:(len(palindrome)//2)+1])
+# 13
+# >>> print(palindrome[len(palindrome)//2:len(palindrome)])
+# 31
+import ListNode
+
+def isPalindrome(head: ListNode) -> bool:
+    # for now we can go with approach that checks the first half of the string
+    # if number is odd
+    #     we evaluate the palindrome of the odd number one way
+    # else
+    #     we slice the string exactly in half
+    pass
