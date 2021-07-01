2081d8c0891c3c9985cdaf8ecf2720a30f71ad4d
Faizan Jamil <faizan.jamilwork@gmail.com>
2021-07-01 19:25:21 -0400

    added reverse function in ListNode

--- coding_problems/LC/ListNode.py
+++ coding_problems/LC/ListNode.py
@@ -11,3 +11,12 @@ class ListNode:
         new_node = ListNode(data)
         self.next = new_node
         return self
+    def reverseLinkedList(self):
+        ll_as_array = []
+        while(self.next != None):
+            ll_as_array.append(self.data)
+            self = self.next
+        ll_reversed = ListNode(ll_as_array[len(ll_as_array)-1])
+        for i in reversed(range(len(ll_as_array)-1)):
+            ll_reversed.addNode(ll_as_array[i])
+        return ll_reversed
