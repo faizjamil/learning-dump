9e54529b1ddbe0766e6dae3be7ded656744d2217
Faizan Jamil <faizan.jamilwork@gmail.com>
2021-08-05 15:40:45 -0400

    started docker tutorial

---
+++ coding_problems/HR/billCounter.js
@@ -0,0 +1,14 @@
+// say we have an arbitrary amount of cash
+// we need to find the optimal amount of two, five, and ten dollar bills to distribute for that amount of cash
+// for example, if we have 42 dollars in cash, we would want to distribute 4 tens and one 2 bill
+
+function billCounter(cash) {
+
+
+    return {
+        two: 0, 
+        five: 0,
+        ten: fif
+    }
+}
+console.log(billCounter(cash).ten)
