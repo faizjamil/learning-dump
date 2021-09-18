c2e34ac3d7f43ab6bf650e93a5cd44151b874176
Faizan Jamil <faizan.jamilwork@gmail.com>
2021-09-18 15:08:52 -0400

    added mock backend and fetch data from it

--- learning-react/react-task-tracker/src/App.js
+++ learning-react/react-task-tracker/src/App.js
@@ -2,24 +2,25 @@
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
 import AddTask from "./components/AddTask";
-import { useState } from "react"
+import { useState, useEffect } from "react"
 
 function App() {
-  const [showAddTask, setShowAddTask] = useState(true)
-  const [tasks, setTasks] = useState([
-    {
-      id: 1,
-      text: 'Doctors Appointment',
-      day: 'Feb 5th at 2:30pm',
-      reminder: true,
-    },
-    {
-      id: 2,
-      text: 'Meeting at School',
-      day: 'Feb 6th at 1:30pm',
-      reminder: true,
+  const [showAddTask, setShowAddTask] = useState(false)
+  const [tasks, setTasks] = useState([])
+  useEffect(() => {
+    const getTasks = async () => {
+      const tasksFromServer = await fetchTasks()
+      setTasks(tasksFromServer)
     }
-  ])
+    getTasks()
+  })
+  // Fetch Tasks
+  const fetchTasks = async () => {
+    const res = await fetch('http://localhost:5000/tasks')
+    const data = await res.json()
+    return data
+  }
+
   // Add Task
   const addTask = (task) => {
     const id = Math.floor(Math.random() * 1000) + 1
@@ -39,7 +40,7 @@ function App() {
   }
   return (
     <div className='Container'>
-      <Header />
+      <Header onAdd={() => setShowAddTask(!showAddTask) }showAdd={showAddTask}/>
       {/* && is shorter way of using a ternary operator without an else */}
       {showAddTask && <AddTask onAdd={addTask}/>}
       {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/> : 'No Tasks To Show'}
--- learning-react/react-task-tracker/src/components/Header.js
+++ learning-react/react-task-tracker/src/components/Header.js
@@ -1,21 +1,19 @@
 import PropTypes from 'prop-types'
 import Button from './Button'
 
-const header = ({title}) => {
-  const onClick = () => {
-    console.log('click')
-  }
+const header = ({title, onAdd, showAdd}) => {
+  
   return (
     <header className='header'>
       <h1>{title}</h1>
-      <Button color='green' text='Add' onClick={onClick}/>
+      <Button color={showAdd ? 'red' : 'green'} text={showAdd ? 'Close' : 'Add'} onClick={onAdd}/>
     </header>
   )
 }
 
 header.defaultProps = {
   title: 'Test defaults',
-
+  showAdd: false
 }
 header.propTypes = {
   title: PropTypes.string
