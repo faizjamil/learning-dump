a76d7dc83248894909930fa7f2072b6ad3292032
Faizan Jamil <faizan.jamil@newsday.com>
2021-11-01 09:42:57 -0400

    trying new tutorial on file upload in express

---
+++ web/upload-file-express-not-working/middleware/upload.js
@@ -0,0 +1,22 @@
+const multer = require('multer')
+const util = require('util');
+const maxSize = 2 * 1024 * 1024;
+
+let storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, __basedir + '/resources/static/assets/uploads/');
+    },
+    filename: (req, file, cb) => {
+        console.log(file.originalname);
+        cb(null, file.originalname)
+    },
+});
+
+let uploadFile = multer({
+    storage: storage,
+    limits: { fileSize: maxSize },
+}).single('file')
+
+let uploadFileMiddleware = util.promisify(uploadFile)
+
+module.exports = uploadFileMiddleware;
