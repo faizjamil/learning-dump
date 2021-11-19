c337af653eb4d3d3696bd4fe6ebc289d4080cb7f
Faizan Jamil <faizan.jamil@newsday.com>
2021-11-19 10:05:38 -0500

    added barebones docs on useage

--- web/s3-client-test/s3_delete_object.js
+++ web/s3-client-test/s3_delete_object.js
@@ -1,3 +1,8 @@
+// call this file from the commandline
+// node s3_delete_object.js <key of file you want to delete>
+// for the sake of flexibility and privacy, the code checks for the s3 bucket url to be specified in an environment variable:
+// S3_BUCKET: <bucket url> DO NOT INCLUDE HTTP/HTTPS:// at the beginning
+
 import { DeleteObjectCommand} from "@aws-sdk/client-s3";
 import { s3Client } from "./s3_client.js";
 const S3_BUCKET = process.env.S3_BUCKET
