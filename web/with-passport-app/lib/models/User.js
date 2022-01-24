27ec9b07cb04423e2daa96e8c5d755b267ddbe32
Faizan Jamil <faizan.jamil@newsday.com>
2022-01-24 11:27:24 -0500

    got mongo working with passport and next-connect

---
+++ web/with-passport-app/lib/models/User.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose')
+
+const UserSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true
+    },
+    hash: {
+        type: String,
+        required: true
+    },
+    salt: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    },
+});
+
+const User = mongoose.model('User', UserSchema);
+module.exports = User;
+// export {
+//     User
+// }
