189216cb5400c4fee919631af974ce931bf76ec6
Faizan Jamil <6825546+faizjamil@users.noreply.github.com>
2026-08-21 15:30:25 -0400

    config and run oxfmt

--- web/next-js/for-testing/next.config.ts
+++ web/next-js/for-testing/next.config.ts
@@ -1,9 +1,9 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  /* Config options here */
   reactCompiler: true,
-  reactStrictMode: true,
+  reactStrictMode: true
 };
 
 export default nextConfig;
